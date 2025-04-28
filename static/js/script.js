b03faf02c8895afafb931e6dfa46ec20260f0207
Thetclash <74104313+Thetclash@users.noreply.github.com>
2025-04-28 19:01:09 +0700

    Update script.js
    menanmbahkan javascript rediret WhatsApp

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,6 @@
-console.log("Website Loaded - Web3 Style Activated!");
+function redirectWA(serviceName) {
+    const phoneNumber = "6282253823713"; // Ganti dengan nomor WA Anda
+    const message = encodeURIComponent(`Halo, saya tertarik dengan layanan ${serviceName}. Bisa dibantu?`);
+    const waUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.location.href = waUrl;
+}
